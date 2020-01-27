018c65ca56df5a25060022c06d5883f0642728b6
saminaa <noemie.lecherf@epsi.fr>
2020-01-27 07:48:42 +0000

    grgswcf

--- src/controleur/controleur_inscription.php
+++ src/controleur/controleur_inscription.php
@@ -15,9 +15,11 @@ function actionInscription($twig, $db){
          $form['message'] = 'Les mots de passe sont différents';
     }
     else{
+        
         $utilisateur = new Utilisateur($db);  //pr mettre en memoire la variable utilisateur
-        $exec = $utilisateur->insert($inputEmail, password_hash($inputPassword, PASSWORD_DEFAULT), $role, $nom, $prenom);  //on lui donne different parametre récupéré dans le formulaire (methode insert(définie dans la classe))
-        if (!$exec){   //si l'execution a échoué                        //password_hash pr hacher le mdp
+        $idUnique = uniqid();
+        $exec = $utilisateur->insert($inputEmail, password_hash($inputPassword, PASSWORD_DEFAULT), $role, $nom, $prenom, 'false', $idUnique);  //on lui donne different parametre récupéré dans le formulaire (methode insert(définie dans la classe))
+        if (!$exec){   //si l'execution a échoué                        //password_hash pr hacher le mdp    false c pr dire qu'au debut l'email n'est pas encore valider
             $form['valide'] = false;
             $form['message'] = 'Problème d\'insertion dans la table utilisateur ';
             }
